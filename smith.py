776abad5ea57a5707ba38847c14d7e3e5c106e11
Toru Shiozaki <shiozaki@northwestern.edu>
2013-07-05 19:10:21 -0500

    This program is under GPL

--- smith.py
+++ smith.py
@@ -1,3 +1,24 @@
+##
+## Filename: smith.py
+## Copyright (C) 2008-2009 Toru Shiozaki
+##
+## Author: Toru Shiozaki <shiozaki.toru@gmail.com>
+##
+## The smith1 program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU Library General Public License as published by
+## the Free Software Foundation; either version 2, or (at your option)
+## any later version.
+##
+## The SMITH2 package is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU Library General Public License for more details.
+##
+## You should have received a copy of the GNU Library General Public License
+## along with the SMITH2 package; see COPYING.  If not, write to
+## the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+##
+
 import sys
 class Sign:
     def __init__(self,sign=1):
